f8ee236276830d4f2bf3b1355f2b796ca808f159
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-08-28 00:00:54 +0530

    done sprial order matrix

---
+++ 04_arrays/22_spiral_order_mat.py
@@ -0,0 +1,45 @@
+def print_matrix(mat):
+    n = len(mat)
+    m = len(mat)
+    for i in range(0, n):
+        for j in range(0, m):
+            print(mat[i][j], end=" ")
+        print()
+    print()
+
+def print_spiral_order(mat):
+    n = len(mat)
+    m = len(mat[0])
+    
+    top = 0
+    left = 0
+    right = m - 1
+    bottom = n - 1
+
+    while top <= bottom and left <= right:
+
+        for i in range(left, right+1):
+            print(mat[top][i], end=" ")
+        top += 1
+
+        for i in range(top, bottom+1):
+            print(mat[i][right], end=" ")
+        right -= 1
+
+        if left <= right:
+            for i in range(right, left-1, -1):
+                print(mat[bottom][i], end=" ")
+            bottom -= 1
+        
+        if top <= bottom:
+            for i in range(bottom, top-1, -1):
+                print(mat[i][left], end=" ")
+            left += 1
+
+    print()
+
+
+mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+print_matrix(mat)
+print_spiral_order(mat)
