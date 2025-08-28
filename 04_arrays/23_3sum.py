ba59dd427bffef706c6c6be84ee3ece90f59b735
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-08-28 23:56:27 +0530

    solved 3sum problem with 2nd better approach

--- 04_arrays/23_3sum.py
+++ 04_arrays/23_3sum.py
@@ -26,4 +26,28 @@ n = len(arr)
 #         temp.add(arr[j])
 # print(res)
 
-# optimal
+# optimal O(nlogn)+O(n2)->S(1)
+arr.sort()
+res = set()
+for i in range(n):
+    if i != 0 and arr[i] == arr[i-1]:
+        continue
+    j = i+1
+    k = n-1
+    while j<k:
+        sum = arr[i]+arr[j]+arr[k]
+        if sum < 0:
+            j += 1
+        elif sum > 0:
+            k -= 1
+        else:
+            res.add(tuple([arr[i],arr[j],arr[k]]))
+            j+=1
+            k-=1
+            while j < k and arr[j]==arr[j+1]:
+                j+=1
+            while j < k and arr[k]==arr[k-1]:
+                k-=1
+print(res)
+            
+
