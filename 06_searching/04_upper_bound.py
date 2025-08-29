89d7bf6832ce48a1ed38d3c2e3c038835e52f4ac
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-08-29 20:23:53 +0530

    implemented upper bound

--- 06_searching/04_upper_bound.py
+++ 06_searching/04_upper_bound.py
@@ -1,13 +1,13 @@
 nums = [1,1,1,2,3,3,5,6,7,7,7,9,12,12,13]
-lb = len(nums)
-target = -2
+up = len(nums)
+target = 9
 low = 0
 high = len(nums)-1
 while high >= low:
     mid = (high+low)//2
-    if nums[mid] >= target:
-        lb = mid
+    if nums[mid] > target:
+        up = mid
         high = mid - 1
     else:
         low = mid + 1
-print(lb)
+print(up)
