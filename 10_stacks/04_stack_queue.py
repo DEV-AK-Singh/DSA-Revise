34e173735bd48b561d636779ada301ecf8d4db2f
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-09-18 15:39:58 +0530

    done stack using queue

---
+++ 10_stacks/04_stack_queue.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.stack = deque([])
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()        
+
+    def top(self) -> int:
+        return self.stack[-1] if len(self.stack) > 0 else -1
+
+    def empty(self) -> bool:
+        return True if len(self.stack) > 0 else False
