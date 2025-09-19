15918ae0210a12111f96e3910f9ef73fa2ad04e9
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-09-19 16:28:09 +0530

    done next greater element problem

--- 10_stacks/09_min_stack.py
+++ 10_stacks/09_min_stack.py
@@ -3,7 +3,10 @@ class MinStack:
         self.stack = [] 
 
     def push(self, val: int) -> None: 
-        self.stack.append([val, min(val, self.stack[-1][1] if self.stack else val)])
+        if len(self.stack) == 0:
+            self.stack.append([val, val])
+        else:
+            self.stack.append([val, min(val, self.stack[-1][1])])
 
     def pop(self) -> None:
         return self.stack.pop()
