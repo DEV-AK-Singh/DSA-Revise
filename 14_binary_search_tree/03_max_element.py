9a8318bf5358d7c5697cf72b73dc531d0b2e731f
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-09-26 23:28:35 +0530

    done bst max element problem

---
+++ 14_binary_search_tree/03_max_element.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+a = Node(9)
+b = Node(3)
+c = Node(11)
+d = Node(2)
+e = Node(7)
+f = Node(15)
+g = Node(14)
+h = Node(16)
+
+a.left = b
+a.right = c
+
+b.left = d
+b.right = e
+
+c.right = f
+
+f.left = g
+f.right = h
+
+#            a 9
+#         /       \
+#       b 3        c 11
+#      /   \          \
+#    d 2   e 7       f 15
+#                    /   \
+#                   g 14   h 16
+
+def max_element(node):
+    if not node:
+        return []
+    while node.right:
+        node = node.right
+    return node.val
+
+print(max_element(a))
